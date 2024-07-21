412e7335b010e04233249909d065a0269458d4db
David Thievon <pdkfan@gmail.com>
2024-07-21 22:54:50 +0200

    Add mappings for finding erros reported by lsps

--- lua/mappings/lsp.lua
+++ lua/mappings/lsp.lua
@@ -6,3 +6,6 @@ vim.api.nvim_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 vim.api.nvim_set_keymap("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+
+vim.api.nvim_set_keymap("n", "ge", "<cmd> lua vim.diagnostic.goto_next({ source = 'workspace' })<CR>", opts)
+vim.api.nvim_set_keymap("n", "gE", "<cmd> lua vim.diagnostic.goto_prev({ source = 'workspace' })<CR>", opts)
