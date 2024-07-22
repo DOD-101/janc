58700119326ce6064ac96dc60009abecd197e847
David Thievon <pdkfan@gmail.com>
2024-07-22 19:45:16 +0200

    Replace undefined opts with {}

--- lua/lazy-nvim.lua
+++ lua/lazy-nvim.lua
@@ -1,27 +1,27 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {}
 
 local function merge(plugin)
-    -- Iterate over each element in the table
-    for _, value in ipairs(plugin) do
-        -- Add each element from the table to M
-        table.insert(plugins, value)
-    end
+	-- Iterate over each element in the table
+	for _, value in ipairs(plugin) do
+		-- Add each element from the table to M
+		table.insert(plugins, value)
+	end
 end
 
 merge(require("plugins"))
 
 table.insert(plugins, { dir = "/home/david/.config/nvim/nvim-testing/lua/themes/ocean_breeze", lazy = true })
-require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins, {})
