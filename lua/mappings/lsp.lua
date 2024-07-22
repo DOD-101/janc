1951f6cc7ff52fd07e7922ce1b74c7f10a12f7a9
David Thievon <pdkfan@gmail.com>
2024-07-22 21:46:36 +0200

    Add better error jumping
    Will now only jump to warns and errors if they exist. Otherwise it will
    jump to hints and infos.

--- lua/mappings/lsp.lua
+++ lua/mappings/lsp.lua
@@ -1,3 +1,22 @@
+local SEVERITY_FILTER_LEVEL = "WARN"
+
+local function severity_filter(diagnostics, severity_level)
+	local severity_nums = {
+		ERROR = 1,
+		WARN = 2,
+		INFO = 3,
+		HINT = 4,
+	}
+
+	for _, diagnostic in ipairs(diagnostics) do
+		if diagnostic.severity <= severity_nums[severity_level] then
+			return true
+		end
+	end
+
+	return false
+end
+
 vim.keymap.set("n", "gd", function()
 	vim.lsp.buf.definition()
 end)
@@ -23,9 +42,25 @@ vim.keymap.set("n", "gr", function()
 end)
 
 vim.keymap.set("n", "ge", function()
-	vim.diagnostic.goto_next({ source = "workspace", severity = { min = vim.diagnostic.severity.WARN } })
+	local diagnostics = vim.diagnostic.get()
+
+	local warns = severity_filter(diagnostics, SEVERITY_FILTER_LEVEL)
+
+	if warns then
+		vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity[SEVERITY_FILTER_LEVEL] } })
+	else
+		vim.diagnostic.goto_next({})
+	end
 end)
 
 vim.keymap.set("n", "gE", function()
-	vim.diagnostic.goto_prev({ source = "workspace" })
+	local diagnostics = vim.diagnostic.get()
+
+	local warns = severity_filter(diagnostics, SEVERITY_FILTER_LEVEL)
+
+	if warns then
+		vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity[SEVERITY_FILTER_LEVEL] } })
+	else
+		vim.diagnostic.goto_prev({})
+	end
 end)
