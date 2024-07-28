6b2d988701bb3ca4fbceca2b643b09621486a90e
David Thievon <pdkfan@gmail.com>
2024-07-28 21:32:25 +0200

    fix(format): format mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,11 @@
 local plugin = {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        },
-        -- Mason and other language features are configured in langs/
+	"williamboman/mason.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	-- Mason and other language features are configured in langs/
 }
 
 return plugin
