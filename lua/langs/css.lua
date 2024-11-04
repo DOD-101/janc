c24444bcddec0fc8e0c87d926a62e79ee83fe278
David Thievon <pkfan@gmail.com>
2024-11-04 12:46:07 +0100

    feat(css): add css lsp

--- lua/langs/css.lua
+++ lua/langs/css.lua
@@ -10,9 +10,16 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
 	pattern = lang.pattern,
 	once = true,
 	callback = function()
+		local lspconfig = require("lspconfig")
 		require("langs.install").ensure_installed("stylelint_lsp", function()
-			local lspconfig = require("lspconfig")
 			lspconfig.stylelint_lsp.setup({
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				filetypes = { "css", "scss" },
+			})
+		end)
+
+		require("langs.install").ensure_installed("cssls", function()
+			lspconfig.cssls.setup({
 				capabilities = require("cmp_nvim_lsp").default_capabilities(),
 				filetypes = { "css", "scss" },
 			})
