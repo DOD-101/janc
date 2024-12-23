31ca3250ba877fcb2383f3e35a196222bcb0fad6
David Thievon <pdkfan@gmail.com>
2024-12-23 09:53:51 +0100

    refactor(langs): layout basic Lang type
    Also implement it for lua and json

--- lua/langs/json.lua
+++ lua/langs/json.lua
@@ -1,36 +1,18 @@
+---@class Lang
 local lang = {
 	lang = "json",
+	filetype = "json",
 	formatter = { "biome" },
 	treesitter = "json",
 	linters = { "biomejs" },
-	pattern = "*.json",
+	lsp = {
+		name = "jsonls",
+		settings = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
+		},
+	},
+	pattern = { "*.json" },
 }
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-	pattern = lang.pattern,
-	once = true,
-	callback = function()
-		require("langs.install").ensure_installed("jsonls", function()
-			local lspconfig = require("lspconfig")
-
-			lspconfig.jsonls.setup({
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				settings = {
-					schemas = require("schemastore").json.schemas(),
-					validate = { enable = true },
-				},
-				filetypes = { "json" },
-			})
-		end)
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = lang.pattern,
-	once = false,
-	callback = function()
-		require("lint").try_lint(lang.linters)
-	end,
-})
 
 return lang
