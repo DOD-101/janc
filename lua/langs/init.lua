14c5a579f4add6a0febe4cd6fb9e448762e441f4
David Thievon <pdkfan@gmail.com>
2024-12-23 09:53:51 +0100

    refactor(langs): layout basic Lang type
    Also implement it for lua and json

--- lua/langs/init.lua
+++ lua/langs/init.lua
@@ -1,3 +1,19 @@
+---@class Lang A class representing all the config data for a language
+---@field lang string The name of the language
+---@field filetype string The vim file type
+---@field formatter table A table of formatters to run on the buffer after saving
+---@field treesitter string The name of the treesitter for the language
+---@field linters table | nil An optional table of linters to run on the buffer
+---@field lsp lspConf Config relating to the LSP for the language
+---@field pattern table A table of patterns for file extensions for the lang
+---@field noSetup boolean | nil Weather to do the default setup for the lang
+---@field tabsize number | nil The tabsize in. `nil` will leave this as the default.
+
+---@class lspConf A class representing all the config for an LSP
+---@field name string The name of the LSP
+---@field settings table The settings table used in the setup function
+---@field callback function | nil A callback to override the default setup function
+
 local langs = {
 	require("langs.lua"),
 	require("langs.html"),
@@ -26,6 +42,39 @@ for _, lang in ipairs(langs) do
 			end,
 		})
 	end
+
+	if lang.noSetup then
+		goto continue
+	end
+
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
+		pattern = lang.pattern,
+		once = true,
+		callback = function()
+			require("langs.install").ensure_installed(lang.lsp.name, lang.lsp.callback or function()
+				local lspconfig = require("lspconfig")
+
+				lspconfig[lang.lsp.name].setup({
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+					settings = lang.lsp.settings,
+					filetypes = { lang.filetype },
+				})
+			end)
+		end,
+	})
+
+	if lang.linters then
+		--- TODO: We could try being more agressive with the linting here
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			pattern = lang.pattern,
+			once = false,
+			callback = function()
+				require("lint").try_lint(lang.linters)
+			end,
+		})
+	end
+
+	::continue::
 end
 
 require("mason").setup({
--- lua/langs/lua.lua
+++ lua/langs/lua.lua
@@ -1,41 +1,24 @@
+---@type Lang
 local lang = {
 	lang = "lua",
 	formatter = { "stylua" },
 	treesitter = "lua",
+	filetype = "lua",
 	linters = { "luacheck" },
-	pattern = "*.lua",
-}
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-	pattern = lang.pattern,
-	once = true,
-	callback = function()
-		require("langs.install").ensure_installed("lua_ls", function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				filetypes = { "lua" },
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						telemetry = {
-							enable = false,
-						},
-					},
+	lsp = {
+		name = "lua_ls",
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
 				},
-			})
-		end)
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-	pattern = lang.pattern,
-	once = false,
-	callback = function()
-		require("lint").try_lint(lang.linters)
-	end,
-})
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	pattern = { "*.lua" },
+}
 
 return lang
