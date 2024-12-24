500cc0e527bb24568c2c976af8cfe122a8b0ff43
David Thievon <pdkfan@gmail.com>
2024-12-24 12:35:01 +0100

    feat(telescope): improve telescope config
    1. Add opening entry in split
    2. Add mappings for git related pickers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,13 +11,63 @@ local plugin = {
 		},
 	},
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		telescope.setup({
 			defaults = {
 				file_ignore_patterns = { "%.git/.*", "target" },
 				layout_config = {
 					horizontal = {
 						width = 0.8,
 						preview_width = 0.5,
+					},
+				},
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--trim",
+				},
+				mappings = {
+					i = {
+						["<C-s>"] = actions.cycle_previewers_next,
+						["<C-a>"] = actions.cycle_previewers_prev,
+						["<M-CR>"] = function(prompt_bufnr)
+							local action_set = require("telescope.actions.set")
+							local action_state = require("telescope.actions.state")
+
+							local picker = action_state.get_current_picker(prompt_bufnr)
+							picker.get_selection_window = function(picker, _)
+								vim.cmd("vsplit")
+								local winnr = vim.api.nvim_get_current_win()
+
+								-- Unbind after using so next instance of the picker acts normally
+								picker.get_selection_window = nil
+								return winnr
+							end
+
+							return action_set.edit(prompt_bufnr, "edit")
+						end,
+						["<S-M-CR>"] = function(prompt_bufnr)
+							local action_set = require("telescope.actions.set")
+							local action_state = require("telescope.actions.state")
+
+							local picker = action_state.get_current_picker(prompt_bufnr)
+							picker.get_selection_window = function(picker, _)
+								vim.cmd("split")
+								local winnr = vim.api.nvim_get_current_win()
+
+								-- Unbind after using so next instance of the picker acts normally
+								picker.get_selection_window = nil
+								return winnr
+							end
+
+							return action_set.edit(prompt_bufnr, "edit")
+						end,
 					},
 				},
 			},
