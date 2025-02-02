8c730adb6612bbec9f812c6807d5517181c49549
David Thievon <pdkfan@gmail.com>
2025-02-02 21:36:56 +0100

    feat(langs): update Langs class
    Use the more concise ?-syntax for optional parameters.

--- lua/langs/init.lua
+++ lua/langs/init.lua
@@ -1,17 +1,17 @@
 ---@class Lang A class representing all the config data for a language
 ---@field name string The name of the language
 ---@field formatters table A table of formatters to run on the buffer after saving
----@field treesitter string | nil Optional name of the treesitter for the language, defaults to the name
----@field linters table | nil An optional table of linters to run on the buffer
+---@field treesitter string? Optional name of the treesitter for the language, defaults to the name
+---@field linters table? An optional table of linters to run on the buffer
 ---@field lsps lspConf[] Config relating to the LSP for the language
 ---@field pattern table A table of patterns for file extensions for the lang
----@field noSetup boolean | nil Weather to do the default setup for the lang
----@field tabsize number | nil The tabsize in. `nil` will leave this as the default.
+---@field noSetup boolean? Weather to do the default setup for the lang
+---@field tabsize number? The tabsize in. `nil` will leave this as the default.
 
 ---@class lspConf A class representing all the config for an LSP
 ---@field name string The name of the LSP
 ---@field settings table The settings table used in the setup function
----@field callback function | nil A callback to override the default setup function
+---@field callback function? A callback to override the default setup function
 
 local langs = {
 	require("langs.lua"),
