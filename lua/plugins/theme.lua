222b4796173f6e2dd6a8732572772a73cc4f98cb
David Thievon <pdkfan@gmail.com>
2025-02-24 17:52:30 +0100

    feat(colorscheme): enable catppuccin integrations

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,6 +9,22 @@ local catppuccin = {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+	---@module 'catppuccin'
+	---@type CatppuccinOptions
+	opts = {
+		integrations = {
+			gitgutter = true,
+			blink_cmp = true,
+			harpoon = true,
+			indent_blankline = {
+				enabled = true,
+				colored_indent_levels = true,
+				scope_color = "maroon",
+			},
+			leap = true,
+			markdown = true,
+		},
+	},
 }
 
 return { tokyonight, catppuccin }
