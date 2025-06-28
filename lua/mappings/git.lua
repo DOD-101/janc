fc5f97e4e98107a38956adf4f005200139fb0a9f
David Thievon <pdkfan@gmail.com>
2025-06-29 00:23:34 +0200

    feat(git): add mapping to change cwd to repo root

--- lua/mappings/git.lua
+++ lua/mappings/git.lua
@@ -13,3 +13,21 @@ vim.keymap.set("n", git_leader .. "n", ":GitGutterNextHunk<CR>")
 vim.keymap.set("n", git_leader .. "p", ":GitGutterPrevHunk<CR>")
 vim.keymap.set("n", git_leader .. "d", ":GitGutterPreviewHunk<CR>")
 vim.keymap.set("n", git_leader .. "u", ":GitGutterUndoHunk<CR>")
+
+vim.keymap.set("n", git_leader .. "w", function()
+	local handle = io.popen("git rev-parse --show-toplevel 2> /dev/null")
+
+	if not handle then
+		vim.cmd("echo 'Failed to get git root dir.'")
+		return
+	end
+
+	local root_dir = handle:read("*l")
+	handle:close()
+	if not root_dir then
+		vim.cmd("echo 'Failed to get git root dir.'")
+		return
+	end
+
+	vim.cmd("cd " .. root_dir)
+end)
