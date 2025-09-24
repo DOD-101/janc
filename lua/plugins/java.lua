a3bbd8fe1b94e9ec062f3a04608f9b36bf23b3b3
David Thievon <pdkfan@gmail.com>
2025-09-24 19:16:19 +0200

    feat(java): replace nvim-java with nvim-jdtls
    nvim-java is far too heavy and does much more than I want it to do.

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,13 +1,10 @@
 ---@module "lazy"
 ---@type LazyPluginSpec
 return {
-	"nvim-java/nvim-java",
-	lazy = true,
+	"mfussenegger/nvim-jdtls",
 	ft = "java",
-	---@module "java"
-	---@type java.Config
-	opts = { jdk = { auto_install = false } },
-	-- setup = function()
-	-- 	require("java").setup()
-	-- end,
+	lazy = true,
+	config = function()
+		require("jdtls")
+	end,
 }
