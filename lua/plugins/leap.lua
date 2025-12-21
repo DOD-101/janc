4fb69a377a20d1d0c9094f2f293f7b125e161039
David Thievon <pdkfan@gmail.com>
2025-12-22 00:20:20 +0100

    feat(leap): lazy load leap on InsertEnter

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,10 +1,11 @@
 return {
 	"ggandor/leap.nvim",
+	lazy = true,
+	event = { "InsertEnter" },
 	dependencies = {
 		"tpope/vim-repeat",
 	},
 	config = function()
-		-- require("octo").setup()
 		require("leap").create_default_mappings()
 	end,
 }
