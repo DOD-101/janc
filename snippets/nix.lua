088718ca279b5ee6d5c986e811cefc36bee68579
David Thievon <david.thievon@proton.me>
2026-01-26 23:04:21 +0100

    feat(snippets): add mkOption snippet for nix

--- snippets/nix.lua
+++ snippets/nix.lua
@@ -2,4 +2,19 @@
 -- luacheck: ignore 113
 return {
 	s("fakeHash", t('"${lib.fakeHash}"')),
+	s({ trig = "mkOption", desc = "Create a new nix option." }, {
+		t("lib.mkOption {"),
+		t("type = lib.types."),
+		i(1),
+		t(";"),
+		t("default = "),
+		i(2),
+		t(";"),
+		t('description = "'),
+		i(3),
+		t('"'),
+		t(";"),
+		t("}"),
+		i(4),
+	}),
 }
