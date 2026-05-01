53417f096f69732dd76aaad4617e8c0fa661d2e7
David Thievon <david.thievon@proton.me>
2026-05-01 20:36:48 +0200

    fix(colors): replace broken hexokinase with nvim-colorizer

--- lua/plugins/colortils.lua
+++ lua/plugins/colortils.lua
@@ -1,7 +1,28 @@
 ---@module "lazy"
----@type LazyPluginSpec
+---@type LazyPluginSpec[]
 return {
-	"max397574/colortils.nvim",
-	cmd = "Colortils",
-	opts = {},
+	{
+		"DeaconDesperado/colortils.nvim",
+		branch = "fix-gradient",
+		cmd = "Colortils",
+		opts = {},
+	},
+	{
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		---@module "colorizer"
+		---@type colorizer.Options
+		---@diagnostic disable: missing-fields
+		opts = {
+			options = {
+				display = {
+					mode = "virtualtext",
+					virtualtext = {
+						char = "󱓻",
+					},
+				},
+			},
+		},
+		---@diagnostic enable: missing-fields
+	},
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
 	require("plugins.comment"),
 	require("plugins.conform"),
 	require("plugins.git"),
-	require("plugins.hexokinase"),
 	require("plugins.java"),
 	require("plugins.leap"),
 	require("plugins.lint"),
