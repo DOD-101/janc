a1a7c6be25ed2192ca0853b747c890ed55d68ecc
David Thievon <david.thievon@proton.me>
2026-06-16 21:31:07 +0200

    refactor(autopairs): use opts

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,7 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = function()
-		require("nvim-autopairs").setup({
-			fast_wrap = {},
-		})
-	end,
+	opts = {
+		fast_wrap = {},
+	},
 }
